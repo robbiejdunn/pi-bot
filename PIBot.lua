993010134c4bbe754469aef03a118a644a791900
Robbie Dunn <robbiejdunn94@gmail.com>
2020-04-09 03:25:36 +0100

    initial stat tracking of pi uptime

--- PIBot.lua
+++ PIBot.lua
@@ -1,5 +1,20 @@
 PIBot = { }
 
+_G["sessionPICastsCount"] = 0
+
+_G["sessionStats"] = {
+    ["DamageSpellCasts"] = 0,
+    ["TotalDamageIncrease"] = 0
+}
+
+-- the unit with PI currently active
+PIBot.PICurrentUnit = nil
+
+-- variables used to track statistics for the current PI cast
+PIBot.PICurrentNumberCasts = 0
+PIBot.PICurrentDamageTotal = 0
+PIBot.PICurrentDamageOverkill = 0
+
 -- local chat_functions = {
 --     ["CD"] = PIB
 -- }
@@ -14,6 +29,8 @@ end
 function PIBot:EventHandler(event, ...)
     if event == "CHAT_MSG_WHISPER" then
         PIBot:ChatEventHandler(...)
+    elseif event == "COMBAT_LOG_EVENT_UNFILTERED" then
+        PIBot:CombatLogEventHandler(...)
     end
 end
 
@@ -25,8 +42,10 @@ function PIBot:ChatEventHandler(msg, sender, ...)
     if string.match(msg, "^pi [a-z]+$") then
         if string.match(msg, " cd$") then
             SendChatMessage(PIBot:Cooldown(), "WHISPER", nil, sender)
+        elseif string.match(msg, " stats$") then
+            SendChatMessage(PIBot:StatsSession(), "WHISPER", nil, sender)
         else
-            SendChatMessage("Unknown command. Available commands are [cd].", "WHISPER", nil, sender)
+            SendChatMessage("Unknown command. Available commands are cd stats.", "WHISPER", nil, sender)
         end
     end
 end
@@ -40,6 +59,41 @@ function PIBot:Cooldown()
         local piCD = string.format("%.2f", piStart + piDuration - GetTime())
         return "PI has a cooldown of " .. piCD .. " seconds."
     end
+end
+
+function PIBot:CombatLogEventHandler(...)
+    timestamp, event, hideCaster, srcGUID, srcName, srcFlags, sourceRaidFlags, dstGUID, dstName, dstFlags, destRaidFlags, arg12, arg13, arg14, arg15, arg16, arg18, arg19, arg20 = CombatLogGetCurrentEventInfo()
+    if srcName == "Dizia" and arg13 == "Inner Fire" and (event == "SPELL_AURA_APPLIED" or event == "SPELL_AURA_REFRESH") then
+        PIBot:PICast(dstName)
+    elseif srcName == PIBot.PICurrentUnit and event == "SPELL_DAMAGE" then
+        PIBot:PISpellDamage(arg15, arg16)
+    elseif event == 'SPELL_AURA_REMOVED' and srcName == PIBot.PICurrentUnit and arg13 == 'Inner Fire' then
+        PIBot:PIEnded()
+    end
+end
+
+function PIBot:PICast(target)
+    PIBot.PICurrentUnit = target
+    _G["sessionPICastsCount"] = _G["sessionPICastsCount"] + 1
+    print("PI cast on " .. PIBot.PICurrentUnit .. "! Casts this session: " .. _G["sessionPICastsCount"])
+end
+
+function PIBot:PISpellDamage(damageTotal, damageOverkill)
+    PIBot.PICurrentNumberCasts = PIBot.PICurrentNumberCasts + 1
+    PIBot.PICurrentDamageTotal = PIBot.PICurrentDamageTotal + damageTotal
+    PIBot.PICurrentDamageOverkill = PIBot.PICurrentDamageOverkill + damageOverkill
+end
+
+function PIBot:StatsSession()
+    return "PI has been cast " .. _G["sessionPICastsCount"] .. " times this session."
+end
+
+function PIBot:PIEnded()
+    print("PI SESSION ENDED")
+
+    local dmgDone = PIBot.PICurrentDamageTotal - PIBot.PICurrentDamageOverkill
+    _G["sessionStats"]["DamageSpellCasts"] = _G["sessionStats"]["DamageSpellCasts"] + PIBot.PICurrentNumberCasts
+    _G["sessionStats"]["TotalDamageIncrease"] = _G["sessionStats"]["TotalDamageIncrease"] + dmgDone
 end
 
 PIBot:Setup()
