1e787361a3f7ea83f30bf895c580f58d680d5a22
Robbie Dunn <robbiejdunn94@gmail.com>
2020-04-09 01:33:43 +0100

    fix event handler

--- PIBot.lua
+++ PIBot.lua
@@ -1,14 +1,14 @@
 PIBot = { }
 
+function PIBot:EventHandler(self, event, ...)
+    print(event)
+end
+
 function PIBot:Setup()
     local frame = CreateFrame("FRAME")
     frame:RegisterEvent("CHAT_MSG_WHISPER")
     frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-    frame:SetScript("OnEvent", PIBot:EventHandler)
-end
-
-function PIBot:EventHandler(event)
-    print(event)
+    frame:SetScript("OnEvent", PIBot.EventHandler)
 end
 
 PIBot:Setup()
